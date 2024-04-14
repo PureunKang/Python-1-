9ec6a7f77d070442fc89b2d001b7dcb55168011d
PureunKang <144876018+PureunKang@users.noreply.github.com>
2024-04-15 02:01:41 +0900

    P02데이터타입

---
+++ 04-Casting.py
@@ -0,0 +1,17 @@
+'''
+0323 오늘 이어서 한 내용
+'''
+
+#논리형으로 타입 변환
+print(bool(10))
+print(bool(-10))
+print(bool(0))
+print(bool('김고은'))
+print(bool(''))
+if 10: #True로 판명.
+    print('조건문')
+if 0: #False로 판명. This code is unreachable
+    print('조건문')
+if '': #False로 판명. This code is unreachable
+    print('조건문')
+
---
+++ 05-intInput.py
@@ -0,0 +1,14 @@
+'''
+# input()으로 입력 받은 문자열을 타입 변환
+저장 받을 변수 = 변환할 타입(input('입력멘트'))
+
+'''
+#정수로 데이터를 입력받기
+numA = int(input('정수 입력: ')) #int로 묶었는데 실수입력하면 에러뜸.
+print('numA: ', type(numA))
+print()
+
+#실수로 데이터를 입력받기
+numB = float(input('실수입력: '))
+print('numB: ', type(numB))
+
