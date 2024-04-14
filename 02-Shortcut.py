33f3ac46e4331ab976501ded0cb24f296d443d8a
PureunKang <144876018+PureunKang@users.noreply.github.com>
2024-04-15 01:56:37 +0900

    Add files via upload
    P00파이참환경

---
+++ 02-Shortcut.py
@@ -0,0 +1,28 @@
+'''
+1. 파이참 단축키 (윈도우 기준)
+파일명 수정: Refactor-Rename / shift+F6
+코드 자동정렬: ctrl + alt + L (원하는 라인 드래그하여)
+한줄(영역) 이동: shift + alt + (화살표 위아래 방향키)
+한줄(영역) 복사: ctrl + D
+한줄(영역) 삭제: ctrl + Y
+다음줄 이동: shift + enter
+열선택하기: ctrl + ctrl + 방향키 위아래
+영역 다중선택: alt + 마우스 클릭
+
+2. 윈도우 공통 단축키
+다시 실행: ctrl + z (앞으로 가기: ctrl+shift+z)
+영역 잘라내기/복사/붙여넣기: ctrl +x/c/v
+전체 선택: ctrl + A
+단어 찾기: ctrl +f  (ctrl+shift+f : 한 프로젝트 안에 변수 찾아서 한꺼번에 바꾸기 가능)
+
+한줄 맨앞뒤 이동: home, end
+단어 단위 이동: ctrl + 화살표 양옆
+단어 단위 삭제: ctrl + delete/backspace
+
+======================================
+gdb online: 온라인 프로그래밍
+구글 코랩
+
+'''
+print(daljfl)
+
